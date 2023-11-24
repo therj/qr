b707f6589cda48e7aef5854341782a68cbb5b631
Rabindra Joshi <14107726+therj@users.noreply.github.com>
2023-11-24 06:26:43 +0545

    setup project

---
+++ .eslintrc.js
@@ -0,0 +1,127 @@
+const { resolve } = require(`node:path`);
+
+const project = resolve(process.cwd(), `tsconfig.json`);
+
+const customRules = {
+  'no-octal-escape': 0,
+  'import/no-unresolved': `error`,
+  'import/extensions': [
+    `error`,
+    `never`,
+    {
+      ignorePackages: true,
+      pattern: {
+        js: `never`,
+        ts: `never`,
+      },
+    },
+  ],
+  'prettier/prettier': [
+    `error`,
+    {
+      trailingComma: `es5`,
+      semi: true,
+      printWidth: 80,
+      singleQuote: true,
+    },
+    { usePrettierrc: false },
+  ],
+  'linebreak-style': [`error`, `unix`],
+  'eol-last': [`warn`, `always`],
+  'no-console': `error`,
+  'no-unused-vars': [`error`, { argsIgnorePattern: `^_` }],
+  'no-underscore-dangle': [`error`, { allow: [`_id`, `_doc`] }],
+  quotes: [
+    `error`,
+    `backtick`,
+    { avoidEscape: true, allowTemplateLiterals: false },
+  ],
+  // "unicorn/filename-case": ["off"],
+  // "@typescript-eslint/explicit-function-return-type": ["off"],
+  // "@typescript-eslint/array-type": ["error", { default: "generic" }],
+};
+
+/*
+ * This is a custom ESLint configuration for use with
+ * Next.js apps.
+ *
+ * This config extends the Vercel Engineering Style Guide.
+ * For more information, see https://github.com/vercel/style-guide
+ *
+ */
+
+const nodeBaseEnvExports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  plugins: [`import`],
+  extends: [`airbnb-base`, `plugin:prettier/recommended`],
+  ignorePatterns: [`node_modules/`, `dist/`, `.turbo`, `.next`],
+  parserOptions: {
+    ecmaVersion: `latest`,
+    project,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  rules: customRules,
+  overrides: [
+    {
+      files: [`**/*.ts`, `**/*.tsx`],
+      extends: [
+        `plugin:@typescript-eslint/recommended`, // Extend TypeScript rules
+      ],
+      parser: `@typescript-eslint/parser`,
+      plugins: [`@typescript-eslint`],
+    },
+  ],
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  plugins: [`import`],
+  extends: [`airbnb-base`, `plugin:prettier/recommended`, `next`],
+  ignorePatterns: [`node_modules/`, `dist/`, `.next`],
+  parserOptions: {
+    ecmaVersion: `latest`,
+    project,
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  // add rules configurations here
+  rules: {
+    ...customRules,
+    'import/no-default-export': `off`,
+    'import/prefer-default-export': `off`,
+  },
+  overrides: [
+    {
+      files: [`**/*.ts`, `**/*.tsx`],
+      extends: [
+        `plugin:@typescript-eslint/recommended`, // Extend TypeScript rules
+      ],
+      parser: `@typescript-eslint/parser`,
+      plugins: [`@typescript-eslint`],
+    },
+  ],
+};
--- README.md
+++ README.md
@@ -6,3 +6,24 @@
 - Save
 
 Hosted on Vercel @ [https://qr.rjoshi.net](https://qr.rjoshi.net)
+
+## Development
+
+### Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+
+### Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
