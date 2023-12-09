7062a067cfd3a4d400f881dfaf96e709bea425cc
Rabindra Joshi <14107726+therj@users.noreply.github.com>
2023-12-10 04:06:50 +0545

    404 and meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import { ThemeProvider } from '@/components/theme-provider';
-import type { Metadata } from 'next';
+import { ThemeProvider, ThemeToggle } from '@/components/theme-provider';
+import { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.scss';
 
@@ -14,8 +14,33 @@ const fontSans = FontSans({
 });
 
 export const metadata: Metadata = {
-  title: app.title,
-  description: app.description,
+  title: {
+    template: `%s | ${app.title}`,
+    default: app.title,
+  },
+  description: app.descriptionLong,
+  applicationName: app.title,
+  other: {
+    'revisit-after': `3 days`,
+  },
+  generator: `Next.js`,
+  referrer: `origin-when-cross-origin`,
+  keywords: [`Mero QR`, `QR Generator`, `QR Scanner Online`],
+  authors: [
+    {
+      name: `Rabindra Joshi`,
+      url: `https://rjoshi.net`,
+    },
+  ],
+  creator: `Rabindra Joshi`,
+  publisher: `Rabindra Joshi`,
+  //
+
+  formatDetection: {
+    email: true,
+    address: true,
+    telephone: true,
+  },
 };
 
 export default function RootLayout({
@@ -24,10 +49,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="h-full" suppressHydrationWarning>
       <body
         className={cn(
-          `min-h-screen bg-background font-sans antialiased`,
+          `h-full min-h-screen bg-background font-sans antialiased`,
           fontSans.variable
         )}
       >
@@ -38,6 +63,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <span className="absolute right-10 top-10">
+            <ThemeToggle />
+          </span>
         </ThemeProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,17 @@
 import { SkeletonDemo } from '@/components/skeleton-demo';
 import { Button } from '@ui/button';
-import { ThemeToggle } from '@/components/theme-provider';
-import pkg from '../package.json';
+import { app } from '@/constants';
 
 export default function Home() {
   return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-4">
-        <h1 className="text-4xl font-bold">Mero QR</h1>
-        <p className="text-sm mb-4">{pkg.description}</p>
-        <Button variant={`default`} size={`lg`} className="animate-bounce">
-          Coming Soon
-        </Button>
-        <SkeletonDemo />
-        <SkeletonDemo />
-      </main>
-      <span className="absolute right-10 top-10">
-        <ThemeToggle />
-      </span>
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-4">
+      <h1 className="text-4xl font-bold">{app.title}</h1>
+      <p className="text-sm mb-4">{app.tagline}</p>
+      <Button size={`lg`} className="animate-bounce">
+        Coming Soon
+      </Button>
+      <SkeletonDemo />
+      <SkeletonDemo />
+    </main>
   );
 }
