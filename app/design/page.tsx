36a70d46212a62cb1de4954b9194e447f4bb7f12
Rabindra Joshi <14107726+therj@users.noreply.github.com>
2023-12-11 01:20:29 +0545

    show home icon on card design

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -1,4 +1,7 @@
 import { QRCard } from '@/components/qr-card';
+import { HomeIcon } from '@radix-ui/react-icons';
+import { Button } from '@ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   const items = [
@@ -15,18 +18,32 @@ export default function Home() {
   const getItemName = () => items[Math.floor(Math.random() * items.length)];
 
   return (
-    <main className="min-h-screen grid grid-cols-3 gap-8  items-centerX justify-center p-24">
-      <QRCard bookmark title="Hello World" />
-      <QRCard bookmark itemName={getItemName()} />
-      <QRCard title="Aditya Was Not Here" />
-      <QRCard title="Unstable Diffusion"></QRCard>
-      <QRCard itemName={getItemName()}></QRCard>
-      <QRCard itemName={getItemName()}></QRCard>
-      <QRCard itemName={getItemName()}></QRCard>
-      <QRCard itemName={getItemName()}></QRCard>
-      <QRCard itemName={getItemName()}></QRCard>
-      <QRCard itemName={getItemName()}></QRCard>
-      <QRCard itemName={getItemName()}></QRCard>
-    </main>
+    <>
+      <main className="min-h-screen grid grid-cols-3 gap-8  items-centerX justify-center p-24">
+        <QRCard bookmark title="Hello World" />
+        <QRCard bookmark itemName={getItemName()} />
+        <QRCard title="Aditya Was Not Here" />
+        <QRCard title="Unstable Diffusion"></QRCard>
+        <QRCard itemName={getItemName()}></QRCard>
+        <QRCard itemName={getItemName()}></QRCard>
+        <QRCard itemName={getItemName()}></QRCard>
+        <QRCard itemName={getItemName()}></QRCard>
+        <QRCard itemName={getItemName()}></QRCard>
+        <QRCard itemName={getItemName()}></QRCard>
+        <QRCard itemName={getItemName()}></QRCard>
+      </main>
+
+      <Button
+        variant="default"
+        size="icon"
+        className="fixed top-px right-2/4 flex items-center border-primary border-0 rounded-full p-2"
+        asChild
+      >
+        <Link href="/">
+          <HomeIcon className="h-4 w-4" />
+          <span className="sr-only">Go Home</span>
+        </Link>
+      </Button>
+    </>
   );
 }
--- src/components/qr-card.tsx
+++ src/components/qr-card.tsx
@@ -125,7 +125,7 @@ export function QRCard({
         </div>
       </CardContent>
       <CardFooter>
-        <Button className="w-full flex items-center">
+        <Button className="w-full flex items-center" asChild>
           <Link href="/" className="flex items-center">
             <HomeIcon className="mr-2 h-4 w-4" /> Go Home
           </Link>
